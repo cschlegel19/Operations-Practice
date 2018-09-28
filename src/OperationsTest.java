b6fd42325779262fa220937bd26d496c5034f3fe
Cooper  Schlegel <42975559+cschlegel19@users.noreply.github.com>
2018-09-28 09:27:36 -0700

    Add files via upload

---
+++ src/OperationsTest.java
@@ -0,0 +1,24 @@
+
+public class OperationsTest
+{
+
+	public static void main(String[] args)
+	{
+		int number; //Declaration statement
+		number = 20; //Assingment statement - initialization
+		number = number + 5; // number+=5
+		System.out.println(number);
+		number/=2;
+		System.out.println(number);
+		number-=6;
+		System.out.println(number);
+		number*=4;
+		System.out.println(number);
+		number%=2;
+		System.out.println(number);
+	
+
+	
+	}
+
+}
